288c239c3587bf80177a0a5ac220856dc4e31bcf
toshiemon18 <st12318@gmail.com>
2015-09-02 01:40:03 +0900

    Add require files

--- lib/syoboemon.rb
+++ lib/syoboemon.rb
@@ -8,6 +8,9 @@ require 'happymapper'
 # syoboemon files
 require "syoboemon/version"
 require "syoboemon/client"
+require "syoboemon/connector"
+require "syoboemon/query_builder/base"
+require "syoboemon/query_builder/program_basic_data"
 
 module Syoboemon
   # Your code goes here...
