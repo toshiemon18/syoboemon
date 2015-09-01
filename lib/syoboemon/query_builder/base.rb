5ceae8849f0e9749b8ab8356379ff4ee414d87b8
toshiemon18 <st12318@gmail.com>
2015-09-01 16:54:07 +0900

    Implement Syoboemon::QueryBuilder::Base class
    ベースクラスを実装した。
    正直無くても良かったけど他のコードでごちゃごちゃになりそうだったのと、パラメータ名のチェックをしておきたいので独立して実装した。

--- lib/syoboemon/query_builder/base.rb
+++ lib/syoboemon/query_builder/base.rb
@@ -1,8 +1,41 @@
 module Syoboemon
 	module QueryBuilder
 		class Base
+
+			attr_reader :params
+
+			OPTIONS = {
+				rss2: [ "user", "filter", "count", "days", "titlefmt" ],
+				db: [ "TID", "LastUpdate", "Fields" ],
+				json: [ "Title", "Limit" ]
+			}
+
 			def initialize(query_params={})
 				@query_params = query_params
+				@query = "?"
+			end
+
+			def assemble_query
+				@query << @query_params.map do |k, v|
+					begin
+						k = determine_params_name_consistency(k)
+						"#{k}=#{v}"
+					rescue => e
+						puts e.message
+						exit 1
+					end
+				end.join("&")
+			end
+
+			private
+
+			def determine_params_name_consistency(attribute, api_type)
+				OPTIONS[api_type.to_sym].each_key do |key|
+					if (/#{attribute}/i === key) then
+						return key
+					end
+				end
+				raise "Specified attribute does not exist."
 			end
 		end
 	end
