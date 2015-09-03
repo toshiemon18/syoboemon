ab3f76c8978251eedad10b0ea846eb86420025d9
toshiemon18 <st12318@gmail.com>
2015-09-03 22:32:26 +0900

    Implemented Syoboemon::Parser::DB and Json clas

---
+++ lib/syoboemon/parser/voice_actors_list.rb
@@ -0,0 +1,28 @@
+module Syoboemon
+	module Parser
+		class DB < Base
+
+			tag "TitleItem"
+			element "TID", String
+			element "Title", String
+			element "Comment", String
+
+		end
+
+		class Json
+
+			def initialize(responce)
+				@parse_data = responce
+			end
+
+			def parse
+				JSON.parse(@parse_data)
+			end
+
+		end
+
+		class VoiceActorsList
+
+		end
+	end
+end
