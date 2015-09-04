322c6c80e416b53a03ac2b50d61aaa31fe5a34e9
toshiemon18 <st12318@gmail.com>
2015-09-04 23:19:09 +0900

    Change to class from module(Syoboemon::Connector)

--- lib/syoboemon/connector.rb
+++ lib/syoboemon/connector.rb
@@ -5,45 +5,43 @@ module Syoboemon
 		DB_PATH		= "/db.php"
 		JSON_PATH	= "/json.php"
 
-		class << self
-			def rss2_get(query)
-				connection.get(rss2_path, query)
-			end
+		def rss2_get(query)
+			connection.get(rss2_path, query)
+		end
 
-			def db_get(query)
-				connection.get(db_path, query)
-			end
+		def db_get(query)
+			connection.get(db_path, query)
+		end
 
-			def json_get(query)
-				connection.get(json_path, query)
-			end
+		def json_get(query)
+			connection.get(json_path, query)
+		end
 
-			private
+		private
 
-			def connection
-				connection ||= Faraday::Connection.new(url: url) do |c|
-					c.request		:url_encoded
-					c.response	:logger
-					c.adapter		:net_http
-					c.response	:raise_error
-				end
+		def connection
+			connection ||= Faraday::Connection.new(url: url) do |c|
+				c.request		:url_encoded
+				c.response	:logger
+				c.adapter		:net_http
+				c.response	:raise_error
 			end
+		end
 
-			def url
-				URL
-			end
+		def url
+			URL
+		end
 
-			def rss2_path
-				RSS2_PATH
-			end
+		def rss2_path
+			RSS2_PATH
+		end
 
-			def db_path
-				DB_PATH
-			end
+		def db_path
+			DB_PATH
+		end
 
-			def json_path
-				JSON_PATH
-			end
+		def json_path
+			JSON_PATH
 		end
 	end
 end
