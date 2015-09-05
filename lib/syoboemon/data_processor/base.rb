73ecc08ab118f222bf4ee746eb00ca657e60e114
toshiemon18 <st12318@gmail.com>
2015-09-05 14:08:57 +0900

    Implement Syoboemon::DataProcessor::Base class

--- lib/syoboemon/data_processor/base.rb
+++ lib/syoboemon/data_processor/base.rb
@@ -1,7 +1,21 @@
 module Syoboemon
 	module DataProcessor
 		class Base
-			
+
+			def initialize(query_params={})
+				unless query_params then
+					@params = query_params
+				else
+					raise "Query　parameter is empty. (Please pass the correct parameters!)"
+				end
+			end
+
+			private 
+
+			def api_connector
+				Connector.new
+			end
+
 		end
 	end
 end
