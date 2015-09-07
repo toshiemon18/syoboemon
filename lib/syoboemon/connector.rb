06ad11043bd97403e83289db463f4042b8627c0f
toshiemon18 <st12318@gmail.com>
2015-09-07 18:44:38 +0900

    Use class_method in Syoboemon::Connector module (add 'class<<self')

--- lib/syoboemon/connector.rb
+++ lib/syoboemon/connector.rb
@@ -5,43 +5,45 @@ module Syoboemon
 		DB_PATH		= "/db.php"
 		JSON_PATH	= "/json.php"
 
-		def rss2_get(query)
-			connection.get(rss2_path, query)
-		end
-
-		def db_get(query)
-			connection.get(db_path, query)
-		end
-
-		def json_get(query)
-			connection.get(json_path, query)
-		end
-
-		private
-
-		def connection
-			connection ||= Faraday::Connection.new(url: url) do |c|
-				c.request		:url_encoded
-				c.response	:logger
-				c.adapter		:net_http
-				c.response	:raise_error
+		class << self
+			def rss2_get(query)
+				connection.get(rss2_path, query)
 			end
-		end
 
-		def url
-			URL
-		end
+			def db_get(query)
+				connection.get(db_path, query)
+			end
 
-		def rss2_path
-			RSS2_PATH
-		end
+			def json_get(query)
+				connection.get(json_path, query)
+			end
 
-		def db_path
-			DB_PATH
-		end
+			private
 
-		def json_path
-			JSON_PATH
+			def connection
+				connection ||= Faraday::Connection.new(url: url) do |c|
+					c.request		:url_encoded
+					c.response	:logger
+					c.adapter		:net_http
+					c.response	:raise_error
+				end
+			end
+
+			def url
+				URL
+			end
+
+			def rss2_path
+				RSS2_PATH
+			end
+
+			def db_path
+				DB_PATH
+			end
+
+			def json_path
+				JSON_PATH
+			end
 		end
 	end
 end
