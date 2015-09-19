e7afa8f6cf23a0cad5a363d931ce57782374c09e
toshiemon18 <st12318@gmail.com>
2015-09-20 05:41:49 +0900

    Fix regular expression

--- lib/syoboemon/program_infomation_accessor/program_detail_search.rb
+++ lib/syoboemon/program_infomation_accessor/program_detail_search.rb
@@ -14,10 +14,10 @@ module Syoboemon
 
   		private
   		def set_up_params_of_structure_members
-        staff_and_cast = split_params_of_comment
+        program_params = split_params_of_comment
   			self.title = @results_of_program.Title
   			self.title_id = @results_of_program.TID
-        self.staffs = set_up_staff_or_cast_params(staff_and_cast[0]) # Set staff
+        self.staffs = set_up_staff_or_cast_params(program_params[0]) # Set staff
   		end
 
       # 取得した番組情報のComment要素よりスタッフ、キャスト、OP、EDのデータを含む文字列を配列にして返す
@@ -27,7 +27,7 @@ module Syoboemon
       # 〜(n-1) => エンディングテーマ（複数ある場合はｓの数だけ）
       # n => キャスト（各キャラクターの配役）
       def split_params_of_comment
-        split_pattern = /"*スタッフ"|"*キャスト"|("オープニング"|"エンディング")*/
+        split_pattern = /"*"スタッフ|"*"キャスト|(オープニング|エンディング)*/
         comment = @results_of_program.Comment
         program_detail_params = comment.split(/"*".+/).map {|elem| elem.match(split_pattern)}
         return program_detail_params
