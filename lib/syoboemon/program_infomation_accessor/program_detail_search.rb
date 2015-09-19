aa0b0d540f63cc9bb0b5f31d50691f9431cbcc48
toshiemon18 <st12318@gmail.com>
2015-09-19 20:55:04 +0900

    Add attributes of structure member

--- lib/syoboemon/program_infomation_accessor/program_detail_search.rb
+++ lib/syoboemon/program_infomation_accessor/program_detail_search.rb
@@ -1,7 +1,29 @@
+# 
+# Syoboemon::ProgramInfomationAccessor::ProgramDetailSearch
+# 
+
+# json.phpとdb.phpを使って検索した番組に関する情報を参照するためのクラス
+
 module Syoboemon
 	module ProgramInfomationAccessor
-		class ProgramDetailSearch
+		class ProgramDetailSearch < Struct.new(:title, :title_id, :staffs, :casts, :opening_themes, :ending_themes)
+			def initialize(parsed_happymapper_object)
+    		@results_of_program = parsed_happymapper_object
+    		set_up_parameter_of_structure_members
+  		end
 
+  		private
+  		def set_up_parameter_of_structure_members
+  			self.title = @results_of_program.Title
+  			self.title_id = @results_of_program.TID
+        self.staffs = 
+  		end
+
+      def split_element_of_comment
+        split_pattern = /"*スタッフ"|"*キャスト"|("オープニング"|"エンディング")*/
+        comment = @results_of_program.Comment
+        staff_and_casts = comment.split("¥n").map {|elem| elem.match()}
+      end
 		end
 	end
 end
